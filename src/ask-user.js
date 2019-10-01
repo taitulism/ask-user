8e503053cb675206526199e15a1e0897e144e31b
Itai Tenenbaum <taitulism@gmail.com>
2019-10-01 20:28:45 +0300

    options stdio

--- src/ask-user.js
+++ src/ask-user.js
@@ -2,12 +2,12 @@ const {createInterface} = require('readline');
 const normalizeTralingSpace = require('./normalize-traling-space');
 const DEFAULT_QUESTION = 'Press "ENTER" to continue... ';
 
-function askUser (question = DEFAULT_QUESTION) {
+function askUser (question, opts = {}) {
 	question = normalizeTralingSpace(question);
 
 	const readline = createInterface({
-		input: process.stdin,
-		output: process.stdout,
+		input: opts.stdin || process.stdin,
+		output: opts.stdout || process.stdout,
 	});
 
 	return new Promise((resolve) => {
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,4 +1,8 @@
+/* eslint-disable object-property-newline */
+
 const {EOL} = require('os');
+const {PassThrough: Stream} = require('stream');
+
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 const expect = chai.expect;
@@ -7,6 +11,7 @@ chai.use(chaiAsPromised);
 const askUser = require('../');
 
 const THE_ULTIMATE_QUESTION = 'What is the answer to life, the universe and everything?';
+const question = `    ${THE_ULTIMATE_QUESTION}\n    > `;
 
 function simulateTyping (text) {
 	const len = text.length;
@@ -22,19 +27,110 @@ function simulateTyping (text) {
 		}
 		else {
 			setTimeout(() => {
-				process.stdin.emit('data', `${EOL}`);
+				process.stdin.emit('data', EOL);
 			}, ++i * 1000);
 		}
 	}
 }
 
+function setAnswerTimeout (stream, text = 'OK') {
+	setTimeout(() => {
+		stream.emit('data', text + EOL);
+	}, 0);
+}
+
 describe('askUser\n  -------', () => {
-	it('waits for user to answer',  () => {
-		simulateTyping('42');
+	describe('Arguments:', () => {
+		describe('[0] String - Question', () => {
+			it('sends the question to `stdout`', () => {
+				const stdin = new Stream();
+				const stdout = new Stream();
 
-		const question = `    ${THE_ULTIMATE_QUESTION}\n    > `;
-		const ask = () => askUser(question);
+				setAnswerTimeout(stdin);
 
-		return expect(ask()).to.eventually.equal('42');
-	}).timeout(5000);
+				let text = '';
+				stdout.on('data', (str) => {
+					text += str;
+				});
+
+				return askUser(question, {stdin, stdout}).then(() => {
+					stdin.destroy();
+					stdout.destroy();
+
+					return expect(text).to.equal(question);
+				});
+			});
+
+			it('adds a trailing space when the question doesn\'t end with a whitespace', () => {
+				const stdin = new Stream();
+				const stdout = new Stream();
+
+				setAnswerTimeout(stdin);
+
+				let text = '';
+				stdout.on('data', (str) => {
+					text += str;
+				});
+
+				const question = 'is it?';
+				const expected = 'is it? ';
+
+				return askUser(question, {stdin, stdout}).then(() => {
+					stdin.destroy();
+					stdout.destroy();
+
+					return expect(text).to.equal(expected);
+				});
+			});
+
+			it('doesn\'t add a trailing space when the question ends with a whitespace', () => {
+				const stdin = new Stream();
+				const stdout = new Stream();
+
+				setAnswerTimeout(stdin);
+
+				let text = '';
+				stdout.on('data', (str) => {
+					text += str;
+				});
+
+				const question = 'is it?\n';
+				const expected = 'is it?\n';
+
+				return askUser(question, {stdin, stdout}).then(() => {
+					stdin.destroy();
+					stdout.destroy();
+
+					return expect(text).to.equal(expected);
+				});
+			});
+		});
+
+		describe('[1] Object - Options', () => {
+			describe('.stdin', () => {
+				it('uses a given stream as `stdin` (default is `process.stdin`)', () => {});
+			});
+
+			describe('.stdout', () => {
+				it('uses a given stream as `stdout` (default is `process.stdout`)', () => {});
+			});
+		});
+	});
+
+	describe('Return', () => {
+		it('returns a promise to the user\'s answer', () => {
+			const stdin = new Stream();
+			const stdout = new Stream();
+			const expectedAnswer = '42';
+
+			setAnswerTimeout(stdin, expectedAnswer);
+
+			return askUser(question, {stdin, stdout}).then((answer) => {
+				stdin.destroy();
+				stdout.destroy();
+
+				return expect(answer).to.equal(expectedAnswer);
+			});
+		});
+	});
 });
